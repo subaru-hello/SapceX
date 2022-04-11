be5b8516ed0712dea09d6e9231894f9f5d0f6558
runteq-profile <octpsubaru@icloud.com>
2022-04-11 10:15:40 +0900

    認証まで終了

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,13 +1,20 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  # ここにスキーマを書きます
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    # Queries for the current user
-    me: User
-  }
+type Query {
+  launches( # replace the current launches query with this one.
+    """
+    The number of results to show. Must be >= 1. Default = 20
+    """
+    pageSize: Int
+    """
+    If you add a cursor here, it will only return results _after_ this cursor
+    """
+    after: String
+  ): LaunchConnection!
+  launch(id: ID!): Launch
+  me: User
+}
 
   type Launch {
     id: ID!
@@ -38,6 +45,7 @@ const typeDefs = gql`
     SMALL
     LARGE
   }
+
   type Mutation {
     # if false, booking trips failed -- check errors
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
@@ -49,9 +57,25 @@ const typeDefs = gql`
   }
 
   type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  """
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection { # add this below the Query type as an additional type.
+  cursor: String!
+  hasMore: Boolean!
+  launches: [Launch]!
+}
+
+type Mission {
+    # ... with rest of schema
+    missionPatch(mission: String, size: PatchSize): String
 }
 `;
 
